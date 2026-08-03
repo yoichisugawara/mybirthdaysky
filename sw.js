b19e7f8393a302f40da2d0092a483e0b93a3b253
yoichisugawara <yoichisugawara@gmail.com>
2026-08-03 18:09:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,16 @@
-const CACHE_NAME = 'birth-sky-v1';
-const urlsToCache = [
-  './index.html',
-  './japan_locations.json',
-  './manifest.json',
-  './icon.svg'
-];
+const CACHE_NAME = 'birth-weather-v1';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
+  // ネットワーク優先のシンプルなキャッシュ戦略
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
